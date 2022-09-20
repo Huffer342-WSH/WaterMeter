d219543f6e1e3121aa47096be82c1db986d8751f
Huffer342-WSH <718007138@qq.com>
2022-09-20 23:09:05 +0800

    增加多通道

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -57,6 +57,18 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, XPT2046_CLK_Pin|XPT2046_CS_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pins : PE2 PE3 PE4 PE6
+                           PE7 PE8 PE9 PE10
+                           PE11 PE12 PE13 PE14
+                           PE15 PE0 PE1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_6
+                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10
+                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
+                          |GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = XPT2046_PENIRQ_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
@@ -68,12 +80,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(XPT2046_MISO_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PBPin PBPin PBPin */
   GPIO_InitStruct.Pin = LCD_DC_Pin|LCD_RST_Pin|XPT2046_MOSI_Pin;
@@ -89,9 +95,33 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PD5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
   /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI2_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI3_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+
   HAL_NVIC_SetPriority(EXTI9_5_IRQn, 1, 0);
   HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
--- Task/Task_Exti.c
+++ Task/Task_Exti.c
@@ -12,30 +12,39 @@
 
 #include "stdio.h"
 #include "string.h"
-WaterMeter_t WaterMeterList[16] = {0};
+WaterMeter_t WaterMeterList[16] =
+{ 0 };
 
-void TaskFunc_Exti(void* argument) {
-    uint32_t tick;
-    uint16_t count=0;
+void TaskFunc_Exti(void *argument)
+{
+	uint32_t tick;
+	uint16_t count = 0;
 
-    for(uint8_t i=0;i<16;i++){
-		sprintf(WaterMeterList[i].userName,"User%hu",i);
+	for (uint8_t i = 0; i < 16; i++)
+	{
+		sprintf(WaterMeterList[i].userName, "User%hu", i);
 		WaterMeterList[i].pulseLimit = 1000;
-		strcpy(WaterMeterList[i].phoneNumber,"13484107382");
-    }
-    for (;;) {
-        osDelay(50);
-        tick = xTaskGetTickCount();
-        for (uint8_t i = 0; i < 16; i++) {
-            WaterMeter_Calculate(&WaterMeterList[i], tick);
-            //            printf("[%hu]Count:%lu\tFreq:%lu\t\n", i, WaterMeterList[i].pulseCnt_prev,
-            //                   WaterMeterList[i].pulseFreq);
-        }
-        if(++count%40==0){
-            printf("%lu\n",WaterMeterList[0].pulseCnt);
-        }
+		strcpy(WaterMeterList[i].phoneNumber, "13484107382");
+	}
+	for (;;)
+	{
+		osDelay(50);
+		tick = xTaskGetTickCount();
+		for (uint8_t i = 0; i < 16; i++)
+		{
+			WaterMeter_Calculate(&WaterMeterList[i], tick);
+			//            printf("[%hu]Count:%lu\tFreq:%lu\t\n", i, WaterMeterList[i].pulseCnt_prev,
+			//                   WaterMeterList[i].pulseFreq);
+		}
+		if (++count % 10 == 0)
+		{
+			for (uint8_t i = 0; i < 16; i++)
+			{
+				printf("[%hu]Count:%lu\tFreq:%lu\t\n", i, WaterMeterList[i].pulseCnt, WaterMeterList[i].pulseFreq);
+			}
+		}
 
-    }
+	}
 }
 
 /**
@@ -44,27 +53,37 @@ void TaskFunc_Exti(void* argument) {
  * ted EXTI line
  * @retval None
  */
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-    uint8_t index = 0;
-    while (GPIO_Pin > 0x0001) {
-        GPIO_Pin >>= 1;
-        index++;
-    }
-    WaterMeterList[0].pulseCnt++;
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	uint8_t index = 0;
+	while (GPIO_Pin > 0x0001)
+	{
+		GPIO_Pin >>= 1;
+		index++;
+	}
+	WaterMeterList[index].pulseCnt++;
 }
 
-void WaterMeter_Clear(WaterMeter_t* wm) {
-    wm->pulseCnt = 0;
-    wm->pulseCnt_prev = 0;
-    wm->pulseFreq = 0;
+void WaterMeter_Clear(WaterMeter_t *wm)
+{
+	wm->pulseCnt = 0;
+	wm->pulseCnt_prev = 0;
+	wm->pulseFreq = 0;
 }
 
-void WaterMeter_Calculate(WaterMeter_t* wm, uint32_t tick) {
-    wm->pulseFreq = 1000 * (wm->pulseCnt - wm->pulseCnt_prev) / (tick - wm->tick_prev);
-    wm->pulseCnt_prev = wm->pulseCnt;
-    wm->tick_prev = tick;
+void WaterMeter_Calculate(WaterMeter_t *wm, uint32_t tick)
+{
+	wm->pulseFreq = 1000 * (wm->pulseCnt - wm->pulseCnt_prev) / (tick - wm->tick_prev);
+	wm->pulseCnt_prev = wm->pulseCnt;
+	wm->tick_prev = tick;
 }
 
-uint32_t WaterMeter_GetCount(WaterMeter_t* wm) { return wm->pulseCnt; }
+uint32_t WaterMeter_GetCount(WaterMeter_t *wm)
+{
+	return wm->pulseCnt;
+}
 
-uint32_t WaterMeter_GetFreq(WaterMeter_t* wm) { return wm->pulseFreq; }
+uint32_t WaterMeter_GetFreq(WaterMeter_t *wm)
+{
+	return wm->pulseFreq;
+}
